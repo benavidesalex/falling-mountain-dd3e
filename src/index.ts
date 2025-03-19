bb38132ac241503ee868934acf0b38bcf09fba6a
benavidesalex <34040042+benavidesalex@users.noreply.github.com>
2025-03-19 13:01:48 -0500

    Update index.ts
    cambio promt gordo trans

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "mondá",
+      prompt: "overweight person at trans party",
     };
 
     const response = await env.AI.run(
